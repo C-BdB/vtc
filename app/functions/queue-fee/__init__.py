dd5d9c72eac3fe7d132df49b525c083cf03890c3
Cyprien BERTRAN DE BALANDA <cyprien.bertrandebalanda@locam168.onmicrosoft.com>
2023-09-15 20:11:04 +0200

    add devops example, infra, and api work

--- app/functions/queue-fee/__init__.py
+++ app/functions/queue-fee/__init__.py
@@ -2,8 +2,14 @@ import azure.functions as func
 import logging
 import os
 from azure.storage.queue import QueueClient
+import pandas as pd
+import pickle as pkl
+import logging
 
-CONNECT_STRING ="AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
 def main(myQueueItem: func.QueueMessage) -> str:
     message = myQueueItem.get_json()
-    logging.info(f"The message is {message}")
+    logging.info(f"The message is {type(message)}")
+    df = pd.DataFrame(message, index=[0])
+    predictor = pkl.load("../training/random_forest.pkl")
+    resp = predictor.predict(df)
+    logging.info(resp)
--- app/functions/request-fee/__init__.py
+++ app/functions/request-fee/__init__.py
@@ -1,13 +1,26 @@
 import azure.functions as func
 import logging
 import os
-from azure.storage.queue import QueueClient
+from azure.storage.queue import QueueClient, BinaryBase64EncodePolicy
+import json
 
-CONNECT_STRING ="AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
-def main(req: func.HttpRequest) -> str:
+QUEUE_CONNECT_STRING = os.environ.get("QUEUE_CONNECT_STRING")
+def main(req: func.HttpRequest, context: func.Context) -> str:
     message = req.get_json()
     logging.info(f"The message is {message}")
     queue_client = QueueClient.from_connection_string(
-        conn_str=CONNECT_STRING, queue_name="fee")
-    queue_client.send_message(message)
-    return message
+        conn_str=QUEUE_CONNECT_STRING, queue_name="fee")
+    message_utf8 = json.dumps((message)).encode('utf-8')
+    queue_client.message_encode_policy = BinaryBase64EncodePolicy()
+    queue_client.send_message(
+        queue_client.message_encode_policy.encode(content=message_utf8))
+    body = {
+        "status": "OK",
+        "request": context.invocation_id
+    }
+    headers = {"Content-Type": "application/json"}
+    return func.HttpResponse(
+        status_code=200,
+        body=json.dumps(body),
+        headers=headers
+    )
